f2766754b5f1d6d47f299747be7c2d5eb55e5bd8
Augusto Ribeiro <augustoribeiro@Augustos-MacBook-Pro.local>
2024-05-23 21:25:29 -0300

    initial commit

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "local" {
+  type = string
+  description = "AWS Local region"
+  default = "us-west-1"
+}
+
+variable "cluster_name" {
+  type = string
+  description = "Nome do Cluster"
+}
